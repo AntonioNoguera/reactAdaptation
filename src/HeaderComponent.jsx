9a797d0860d3be0e38388e37335716a3ac91c339
AntonioNoguera <michael.noguerag@uanl.edu.mx>
2023-11-08 22:52:12 -0600

    span distribution with buttons

--- src/HeaderComponent.jsx
+++ src/HeaderComponent.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 function HeaderComp({datos}){
 
     const celdas = datos.map((item,index)=>(
-
         <th className={`px-1 text-center dia${index}`} key={index}>{item}</th> 
     ));
 
--- src/RowComponent.jsx
+++ src/RowComponent.jsx
@@ -3,21 +3,42 @@ import React from "react";
 var firstStrike=true;
 var day="Default";
 var alternator=0;
-
 var globalColor=1;
+
+const asd ="";
+
 function TurnRow(props,turno,rowClass){
-  var rows = [];
-  
-  props.data.food_time.forEach((item, index) => { 
+  var rows = []; 
+  console.log(turno);
+
+  const handleSVGClick = (id) => {
+    // Aquí puedes mostrar una alerta o realizar cualquier acción que desees
+    alert(`${id}`);
+  };
+  console.log(props.data.day);
+  props.data.food_time.forEach((item, index) => {  
+    const svgPath = (
+      <svg height="24" viewBox="0 -960 960 960" width="24" className="custom-span">
+        <path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z" />
+      </svg>
+    );
 
     if (firstStrike) {
       rows.push(<td className={`dayLabel p-2 align-middle columnDay${globalColor}`} rowSpan={item.turnEspecification.length}>{day}</td>);
-      rows.push(<td className={`align-middle ${rowClass}`}>{item.turnEspecification.find(item => item.food === turno).members}</td>);
+      rows.push( 
+          <td className={`align-middle ${rowClass}`}>{
+            item.turnEspecification.find(item => item.food === turno).members}
+            <span id={`${index},${index}`} className={`botonVolver`} onClick={() => handleSVGClick(`${props.data.day}-${turno}-${index}`)}>{svgPath}</span>
+          </td> 
+      );
       
       globalColor++;
       firstStrike = false;
     }else{
-      rows.push(<td className={`align-middle ${rowClass}`}>{item.turnEspecification.find(item => item.food === turno).members}</td>);
+      rows.push(<td className={`align-middle ${rowClass}`}>{
+        item.turnEspecification.find(item => item.food === turno).members}
+        <span id={`${index},${index}`} className={`botonVolver`} onClick={() => handleSVGClick(`${props.data.day}-${turno}-${index}`)}>{svgPath}</span>
+      </td>);
     }
     
   });
@@ -31,24 +52,20 @@ function TurnRow(props,turno,rowClass){
 }
 
 function RowComponent(props) {
-  var lines =[];
-  console.log(props.data.food_time);
+  var lines =[]; 
   day = props.data.day;
   var x = props.data.food_time[0].turnEspecification;
-  for(var i=0;i<x.length;i++){
-    console.log(props.data.food_time[0].turnEspecification[i].food)
+  for(var i=0;i<x.length;i++){ 
 
     var f = i % 2 === 0 ? "tbGray" : "normal";
 
-    if(i===x.length-1){
-      console.log("Se lanza");
+    if(i===x.length-1){ 
       f+=" lastRow";
     }
 
     lines.push(TurnRow(props,props.data.food_time[0].turnEspecification[i].food,f));
   } 
-  firstStrike=true;
-  console.log(lines);
+  firstStrike=true; 
 
   return(<>
   {lines.map((elemento, index) => (
